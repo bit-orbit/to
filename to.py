9663115d7f017e1c058e0e610edd074470792eae
shabane <m.mohamadshabane@gmail.com>
2022-06-17 22:08:42 +0430

    feat:
    	in the to.py file:
    
    		we create a method which will print all the tasks names and
    		its list name.

--- to.py
+++ to.py
@@ -10,6 +10,9 @@ board = None
 assert os.path.exists(board_path), errors.FILE_NOT_EXIST
 
 with open(board_path, 'r') as fli:
-    board = core.Tools().dict2object(json.load(fli))
+    board = json.load(fli)
+    listed_tasks = core.getAllListedTasks(board)
 
-print(board)
+for listed_task in listed_tasks:
+    for task in listed_tasks.get(listed_task):
+        print(listed_task, task['name'])

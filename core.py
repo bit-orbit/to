f8823e96fc79d80d3ca45b512a9e4ca5a80cab76
shabane <m.mohamadshabane@gmail.com>
2022-06-17 22:07:14 +0430

    refactor:
    	the core was changed.
    	create a module to return list of all the tasks.

--- core.py
+++ core.py
@@ -1,97 +1,11 @@
-class File:
-    def __init__(self, path: str, name: str, cid: str) -> None:
-        self.path = path
-        self.name = name
-        self.cid = cid
 
 
-class Attachment:
-    def __init__(self, image: list, audio: list, archive: list, videos: list, document: list, other: list) -> None:
-        self.image = image
-        self.audio = audio
-        self.archive = archive
-        self.videos = videos
-        self.document = document
-        self.other = other
+def getAllListedTasks(board):
+    """
+    get a dict and return all the listed tasks
+    :param board:
+    :return: dict
+    """
+    return board.get('listed_tasks')
 
 
-class Time:
-    def __init__(self, hour: int, minute: int) -> None:
-        self.hour = hour
-        self.minute = minute
-
-
-class Date:
-    def __init__(self, year: int, month: int, day: int) -> None:
-        self.year = year
-        self.month = month
-        self.day = day
-
-
-class User:
-    def __init__(self, id, name, family, username, position) -> None:
-        self.id = id
-        self.name = name
-        self.family = family
-        self.username = username
-        self.position = position
-
-
-class Taken:
-    def __init__(self, is_taken, user: User, date: Date, time: Time) -> None:
-        self.is_taken = is_taken
-        self.user = user
-        self.date = date
-        self.time = time
-
-
-class Done:
-    def __init__(self, is_finished: bool, user: User, date: Date, time: Time) -> None:
-        self.is_finished = is_finished
-        self.user = user
-        self.date = date
-        self.time = time
-
-
-class Created:
-    def __int__(self, date, time) -> None:
-        self.date = date
-        self.time = time
-
-
-class Deadline:
-    def __init__(self, date, time):
-        self.date = date
-        self.time = time
-
-
-class Mention:
-    def __init__(self, of: User, to: User, date: Date, time: Time) -> None:
-        self.of = of
-        self.to = to
-        self.date = date
-        self.time = time
-
-
-class Task:
-    def __init__(self, id: int, name: str, description: str, priority: int, mentions: list, taken: Taken, created: Created, deadline: Deadline, done: Done, attachment: Attachment) -> None:
-        self.id = id
-        self.name = name
-        self.description = description
-        self.priority = priority
-        self.priority = priority
-        self.mentions = mentions
-        self.taken = taken
-        self.created = created
-        self.deadline = deadline
-        self.done = done
-        self.attachment = attachment
-
-
-class Tools:
-
-    def dict2object(self, dict):
-        for listed_task in dict.get('listed_tasks'):
-            for task in dict.get('listed_tasks').get(listed_task):
-                print(task)
-                #todo: convert a task to a task object
